08d6efadd1295e947b0403dbf9a49176cf1870c3
vkioresku <>
2021-04-06 10:10:31 +0300

    Fix Html.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Document, { Head, Main, NextScript } from 'next/document';
+import Document, { Head, Main, NextScript, Html } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document<any> {
@@ -35,7 +35,7 @@ export default class MyDocument extends Document<any> {
 
   render() {
     return (
-      <html>
+      <Html>
         <Head>
           {this.props.styleTags /*rendering the actually stylesheet*/}
         </Head>
@@ -43,7 +43,7 @@ export default class MyDocument extends Document<any> {
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }

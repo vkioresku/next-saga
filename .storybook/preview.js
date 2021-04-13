496472484431f0913ea66f8c1c8a30450f5d0182
vkioresku <>
2021-04-13 12:32:15 +0300

    Update and connect storybook to MUI.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,33 @@
+import React from 'react';
+import { addDecorator, addParameters } from '@storybook/react';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import * as nextImage from 'next/image';
+
+import ThemeDecorator from './ThemeDecorator';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  options: {
+    storySort: {
+      method: 'alphabetical',
     },
   },
-}
+};
+
+addDecorator((Story) => (
+  <ThemeDecorator>
+    <Story />
+  </ThemeDecorator>
+));
+
+addParameters({
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+  },
+});
+
+// Replace next/image for Storybook
+Object.defineProperty(nextImage, 'default', {
+  configurable: true,
+  value: (props) => <img {...props} />,
+});
